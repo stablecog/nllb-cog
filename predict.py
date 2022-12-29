f70f0b8949e8db2fa133f85a241ff48da93bdf92
yekta <ngyekta@gmail.com>
2022-12-29 09:19:44 +0300

    Secondary string, no arrays for inputs so using a weird solution

--- predict.py
+++ predict.py
@@ -30,53 +30,45 @@ class Predictor(BasePredictor):
     @torch.cuda.amp.autocast()
     def predict(
         self,
-        texts: List[str] = Input(description="Input text.", default=[""]),
-        text_langs: List[str] = Input(description="Input text language code (FLORES-200). It overrides the language auto-detection.", default=None),
-        target_langs: List[str] = Input(description="Target language code (FLORES-200).", default=None),
-        target_lang_max_scores: List[float] = Input(
+        text: str = Input(description="Input text.", default=""),
+        text_lang: str = Input(description="Input text language code (FLORES-200). It overrides the language auto-detection.", default=None),
+        target_lang: str = Input(description="Target language code (FLORES-200).", default="eng_Latn"),
+        target_lang_max_score: float = Input(
             description="Target language max score for language auto-detection. If detected score is higher than this value, it would override the guess to target_lang as opposed to using detected_lang.",
-            default=None
+            default=0.9
         ),
-        labels: List[str] = Input(description="A label for the logs.", default=None),
+        label: str = Input(description="A label for the logs.", default="Text"),
+        text_2: str = Input(description="#2 - Input text.", default=None),
+        text_lang_2: str = Input(description="#2 - Input text language code (FLORES-200). It overrides the language auto-detection.", default=None),
+        target_lang_2: str = Input(description="#2 - Target language code (FLORES-200).", default="eng_Latn"),
+        target_lang_max_score_2: float = Input(
+            description="#2 - Target language max score for language auto-detection. If detected score is higher than this value, it would override the guess to target_lang as opposed to using detected_lang.",
+            default=0.9
+        ),
+        label_2: str = Input(description="#2 - A label for the logs.", default="Text"),
     ) -> List[str]:
-        
-        default_text_lang=None
-        default_target_text_lang = "eng-Latn"
-        default_target_lang_max_score = 0.9
-        default_label = "Text"
-        
-        if text_langs is None:
-            text_langs = [default_text_lang] * len(texts)
-        if target_langs is None:
-            target_langs = [default_target_text_lang] * len(texts)
-        if target_lang_max_scores is None:
-            target_langs = [default_target_lang_max_score] * len(texts)
-        if labels is None:
-            labels = [default_label] * len(texts)
-       
-        if len(texts) != len(text_langs) or len(texts) != len(target_langs) or len(texts) != len(target_lang_max_scores):
-            print("ERROR: texts, text_langs, target_langs, and target_lang_max_scores must be the same length!")
-            raise ValueError(
-                "Texts, text_langs, target_langs, and target_lang_max_scores must be the same length!"
-            )
-
-        output_paths = []
-        for i in range(len(texts)):
-            text = texts[i]
-            text_lang = text_langs[i]
-            target_lang = target_langs[i]
-            target_lang_max_score = target_lang_max_scores[i]
-            label = labels[i]
-            translated_text = translate_text(
-                text,
-                text_lang,
-                target_lang,
-                target_lang_max_score,
+        output_strings = []
+        translated_text = translate_text(
+            text,
+            text_lang,
+            target_lang,
+            target_lang_max_score,
+            self.translate_model,
+            self.translate_tokenizer,
+            self.detect_language,
+            label
+        )
+        output_strings.append(translated_text)
+        if text_2 is not None:
+            translated_text_2 = translate_text(
+                text_2,
+                text_lang_2,
+                target_lang_2,
+                target_lang_max_score_2,
                 self.translate_model,
                 self.translate_tokenizer,
                 self.detect_language,
-                label
+                label_2
             )
-            output_paths.append(translated_text)
-        
-        return output_paths
+            output_strings.append(translated_text_2)
+        return output_strings
