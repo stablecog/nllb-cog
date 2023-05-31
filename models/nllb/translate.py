893bb357347d5113eb91c6990a4bb7e10f81f623
yekta <ngyekta@gmail.com>
2023-05-31 20:52:15 +0300

    Streamline translate function

--- models/nllb/translate.py
+++ models/nllb/translate.py
@@ -1,66 +1,124 @@
 from lingua import Language
 import time
 from transformers import pipeline
-from .constants import LANG_TO_FLORES
+from .constants import (
+    LANG_TO_FLORES,
+)
 import torch
+from tabulate import tabulate
 
 
-def translate_text(text, text_flores, target_flores, target_score_max, model, tokenizer, detector, label):
+def translate_text(
+    text,
+    text_flores,
+    target_lang,
+    target_flores,
+    target_score_max,
+    detected_confidence_score_min,
+    model,
+    tokenizer,
+    detector,
+    label,
+):
     if text == "":
         print(f"-- {label} - No text to translate, skipping --")
         return ""
     startTimeTranslation = time.time()
     translated_text = ""
-    decided_text_flores = target_flores
-    
-    if text_flores != None:
-        decided_text_flores = text_flores
-        print(f'-- {label} - FLORES-200 code is given, skipping language auto-detection: "{decided_text_flores}" --')
-    else:
-        confidence_values = detector.compute_language_confidence_values(text)
-        target_lang_score = None
-        detected_lang = None
-        detected_lang_score = None
-        
-        print(f'-- Confidence values - {confidence_values[:10]} --')
-        for index in range(len(confidence_values)):
-            curr = confidence_values[index]
-            if index == 0:
-                detected_lang = curr[0]
-                detected_lang_score = curr[1]
-            if curr[0] == Language.ENGLISH:
-                target_lang_score = curr[1]
-        
-        detected_lang_flores = LANG_TO_FLORES.get(detected_lang.name)
-        if detected_lang is not None and detected_lang_flores is not None and detected_lang_flores != target_flores and (target_lang_score is None or target_lang_score < target_score_max):
-            decided_text_flores = LANG_TO_FLORES.get(detected_lang.name)
-        
-        if detected_lang is not None:
-            print(f'-- {label} - Guessed text language: "{detected_lang.name}". Score: {detected_lang_score} --')
-        if detected_lang is not None and target_lang_score is not None and detected_lang != target_flores:
-            print(f'-- {label} - Target language score: {target_lang_score} --')
-        
-        print(f'-- {label} - Selected text language FLORES-200: "{decided_text_flores}" --')
+    decided_text_flores = get_flores(
+        text=text,
+        text_flores=text_flores,
+        target_lang=target_lang,
+        target_flores=target_flores,
+        target_score_max=target_score_max,
+        detected_confidence_score_min=detected_confidence_score_min,
+        detector=detector,
+        label=label,
+    )
 
     if decided_text_flores != target_flores:
         translate = pipeline(
-            'translation',
+            "translation",
             model=model,
             tokenizer=tokenizer,
             torch_dtype=torch.float16,
             src_lang=decided_text_flores,
             tgt_lang=target_flores,
-            device=0
+            device=0,
         )
         translate_output = translate(text, max_length=1000)
-        translated_text = translate_output[0]['translation_text']
+        translated_text = translate_output[0]["translation_text"]
         print(f'-- {label} - Original text is: "{text}" --')
         print(f'-- {label} - Translated text is: "{translated_text}" --')
     else:
         translated_text = text
-        print(f"-- {label} - Text is already in the correct language, no translation needed --")
-    
+        print(
+            f"-- {label} - Text is already in the correct language, no translation needed --"
+        )
+
     endTimeTranslation = time.time()
-    print(f"-- {label} - Completed in: {round((endTimeTranslation - startTimeTranslation), 2)} sec. --")
-    
+    print(
+        f"-- {label} - Completed in: {round((endTimeTranslation - startTimeTranslation), 2)} sec. --"
+    )
+
     return translated_text
+
+
+def get_flores(
+    text,
+    text_flores,
+    target_lang,
+    target_flores,
+    target_score_max,
+    detected_confidence_score_min,
+    detector,
+    label,
+):
+    if text == "":
+        print(f"-- {label} - No text to give FLORES-200 for, skipping --")
+        return target_flores
+    if text_flores is not None and text_flores != "":
+        print(
+            f'-- {label} - FLORES-200 code is given, skipping language auto-detection: "{text_flores}" --'
+        )
+        return text_flores
+
+    text_lang_flores = target_flores
+    confidence_values = detector.compute_language_confidence_values(text)
+    target_lang_score = None
+    detected_lang = None
+    detected_lang_score = None
+
+    print(f"-- Confidence values --")
+    print(tabulate(confidence_values[:5]))
+
+    for index in range(len(confidence_values)):
+        curr = confidence_values[index]
+        if index == 0:
+            detected_lang = curr[0]
+            detected_lang_score = curr[1]
+        if curr[0] == Language.ENGLISH:
+            target_lang_score = curr[1]
+
+    if (
+        detected_lang is not None
+        and detected_lang != target_lang
+        and detected_lang_score > detected_confidence_score_min
+        and (target_lang_score is None or target_lang_score < target_score_max)
+        and LANG_TO_FLORES.get(detected_lang.name) is not None
+    ):
+        text_lang_flores = LANG_TO_FLORES[detected_lang.name]
+
+    if detected_lang is not None:
+        print(
+            f'-- {label} - Guessed text language: "{detected_lang.name}". Score: {detected_lang_score} --'
+        )
+    if (
+        detected_lang is not None
+        and target_lang_score is not None
+        and detected_lang != target_lang
+    ):
+        print(f"-- {label} - Target language score: {target_lang_score} --")
+
+    print(f'-- {label} - Selected text language FLORES-200: "{text_lang_flores}" --')
+    return text_lang_flores
--- predict.py
+++ predict.py
@@ -1,11 +1,18 @@
 from typing import List
 
 import torch
-from cog import BasePredictor, Input, Path
+from cog import BasePredictor, Input
 
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-from models.nllb.constants import TRANSLATOR_MODEL_CACHE, TRANSLATOR_TOKENIZER_CACHE, TRANSLATOR_MODEL_ID
+from models.nllb.constants import (
+    TRANSLATOR_MODEL_CACHE,
+    TRANSLATOR_TOKENIZER_CACHE,
+    TRANSLATOR_MODEL_ID,
+    TARGET_LANG,
+    TARGET_LANG_SCORE_MAX,
+    DETECTED_CONFIDENCE_SCORE_MIN,
+)
 from models.nllb.translate import translate_text
 
 from lingua import LanguageDetectorBuilder
@@ -15,14 +22,19 @@ class Predictor(BasePredictor):
     def setup(self):
         """Load the model into memory to make running multiple predictions efficient"""
         print("Loading language detector...")
-        self.detect_language = LanguageDetectorBuilder.from_all_languages().with_preloaded_language_models().build()
+        self.detect_language = (
+            LanguageDetectorBuilder.from_all_languages()
+            .with_preloaded_language_models()
+            .build()
+        )
         print("Loaded language detector!")
-        
+
         print("Loading translator...")
-        self.translate_tokenizer = AutoTokenizer.from_pretrained(TRANSLATOR_MODEL_ID, cache_dir=TRANSLATOR_TOKENIZER_CACHE)
+        self.translate_tokenizer = AutoTokenizer.from_pretrained(
+            TRANSLATOR_MODEL_ID, cache_dir=TRANSLATOR_TOKENIZER_CACHE
+        )
         self.translate_model = AutoModelForSeq2SeqLM.from_pretrained(
-            TRANSLATOR_MODEL_ID,
-            cache_dir=TRANSLATOR_MODEL_CACHE
+            TRANSLATOR_MODEL_ID, cache_dir=TRANSLATOR_MODEL_CACHE
         )
         print("Loaded translator!")
 
@@ -31,44 +43,66 @@ class Predictor(BasePredictor):
     def predict(
         self,
         text: str = Input(description="Input text.", default=""),
-        text_lang: str = Input(description="Input text language code (FLORES-200). It overrides the language auto-detection.", default=None),
-        target_lang: str = Input(description="Target language code (FLORES-200).", default="eng_Latn"),
+        text_lang: str = Input(
+            description="Input text language code (FLORES-200). It overrides the language auto-detection.",
+            default=None,
+        ),
+        target_lang: str = Input(
+            description="Target language code (FLORES-200).", default=TARGET_LANG
+        ),
         target_lang_max_score: float = Input(
             description="Target language max score for language auto-detection. If detected score is higher than this value, it would override the guess to target_lang as opposed to using detected_lang.",
-            default=0.9
+            default=TARGET_LANG_SCORE_MAX,
+        ),
+        detected_confidence_score_min: float = Input(
+            description="#1 - Minimum confidence score for language auto-detection. If detected score is lower than this value, it would override the guess to target_lang as opposed to using detected_lang.",
+            default=DETECTED_CONFIDENCE_SCORE_MIN,
         ),
         label: str = Input(description="A label for the logs.", default="Text"),
         text_2: str = Input(description="#2 - Input text.", default=None),
-        text_lang_2: str = Input(description="#2 - Input text language code (FLORES-200). It overrides the language auto-detection.", default=None),
-        target_lang_2: str = Input(description="#2 - Target language code (FLORES-200).", default="eng_Latn"),
+        text_lang_2: str = Input(
+            description="#2 - Input text language code (FLORES-200). It overrides the language auto-detection.",
+            default=None,
+        ),
+        target_lang_2: str = Input(
+            description="#2 - Target language code (FLORES-200).", default=TARGET_LANG
+        ),
         target_lang_max_score_2: float = Input(
             description="#2 - Target language max score for language auto-detection. If detected score is higher than this value, it would override the guess to target_lang as opposed to using detected_lang.",
-            default=0.9
+            default=TARGET_LANG_SCORE_MAX,
+        ),
+        detected_confidence_score_min_2: float = Input(
+            description="#2 - Minimum confidence score for language auto-detection. If detected score is lower than this value, it would override the guess to target_lang as opposed to using detected_lang.",
+            default=DETECTED_CONFIDENCE_SCORE_MIN,
         ),
         label_2: str = Input(description="#2 - A label for the logs.", default="Text"),
     ) -> List[str]:
         output_strings = []
         translated_text = translate_text(
-            text,
-            text_lang,
-            target_lang,
-            target_lang_max_score,
-            self.translate_model,
-            self.translate_tokenizer,
-            self.detect_language,
-            label
+            text=text,
+            text_flores=text_lang,
+            target_lang=target_lang,
+            target_flores=target_lang,
+            detected_confidence_score_min=detected_confidence_score_min,
+            target_lang_max_score=target_lang_max_score,
+            model=self.translate_model,
+            tokenizer=self.translate_tokenizer,
+            detector=self.detect_language,
+            label=label,
         )
         output_strings.append(translated_text)
         if text_2 is not None:
             translated_text_2 = translate_text(
-                text_2,
-                text_lang_2,
-                target_lang_2,
-                target_lang_max_score_2,
-                self.translate_model,
-                self.translate_tokenizer,
-                self.detect_language,
-                label_2
+                text=text_2,
+                text_flores=text_lang_2,
+                target_lang=target_lang_2,
+                target_flores=target_lang_2,
+                detected_confidence_score_min=detected_confidence_score_min_2,
+                target_lang_max_score=target_lang_max_score_2,
+                model=self.translate_model,
+                tokenizer=self.translate_tokenizer,
+                detector=self.detect_language,
+                label=label_2,
             )
             output_strings.append(translated_text_2)
         return output_strings
